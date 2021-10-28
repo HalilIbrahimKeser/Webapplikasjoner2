f8b83e44430b4388948cf50b6eb129a5a7b9efc3
Halil Ibrahim Keser <hke006@uit.no>
2021-10-28 13:44:58 +0200

    api

---
+++ Oblig2_Blogg/Migrations/20211028114013_endring application user.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Oblig2_Blogg.Migrations
+{
+    public partial class endringapplicationuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropPrimaryKey("PK_AspNetUserTokens", "AspNetUserTokens");
+            migrationBuilder.DropPrimaryKey("PK_AspNetUserLogins", "AspNetUserLogins");
+
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Blogs_BlogId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_AspNetUsers_ApplicationUserId",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Blogs_ApplicationUserId",
+                table: "Blogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_BlogId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "BlogId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "BlogId",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 666, DateTimeKind.Local).AddTicks(4367));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "BlogId",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(2688));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "BlogId",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(2854));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(6254));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(7268));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(7304));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(7327));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(4204));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(5082));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(5124));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 13, 40, 12, 670, DateTimeKind.Local).AddTicks(5148));
+
+            migrationBuilder.AddPrimaryKey("PK_AspNetUserTokens", "AspNetUserTokens", new[] { "UserId", "LoginProvider", "Name" });
+            migrationBuilder.AddPrimaryKey("PK_AspNetUserLogins", "AspNetUserLogins", new[] { "LoginProvider", "ProviderKey" });
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Blogs",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BlogId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "BlogId",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 541, DateTimeKind.Local).AddTicks(1649));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "BlogId",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 548, DateTimeKind.Local).AddTicks(7158));
+
+            migrationBuilder.UpdateData(
+                table: "Blogs",
+                keyColumn: "BlogId",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 548, DateTimeKind.Local).AddTicks(7504));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 549, DateTimeKind.Local).AddTicks(4995));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 549, DateTimeKind.Local).AddTicks(6881));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 549, DateTimeKind.Local).AddTicks(6968));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "CommentId",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 549, DateTimeKind.Local).AddTicks(7019));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 549, DateTimeKind.Local).AddTicks(366));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 549, DateTimeKind.Local).AddTicks(2350));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 549, DateTimeKind.Local).AddTicks(2444));
+
+            migrationBuilder.UpdateData(
+                table: "Posts",
+                keyColumn: "PostId",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2021, 10, 28, 11, 16, 18, 549, DateTimeKind.Local).AddTicks(2498));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_ApplicationUserId",
+                table: "Blogs",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_BlogId",
+                table: "AspNetUsers",
+                column: "BlogId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Blogs_BlogId",
+                table: "AspNetUsers",
+                column: "BlogId",
+                principalTable: "Blogs",
+                principalColumn: "BlogId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_AspNetUsers_ApplicationUserId",
+                table: "Blogs",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
